24b796c5b59c80794ebc2e92326ee20484aea21b
YohangLibet <36272025+YohangLibet@users.noreply.github.com>
2019-05-22 10:23:52 +0800

    change to javascript

--- BinarySearch/BinarySearch.js
+++ BinarySearch/BinarySearch.js
@@ -1,22 +1,20 @@
 function binarySearch(list, item) {
-    var low = 0;
-    var high = list.length - 1;
-    var mid;
-    var guess;
-    while (low <= high) {
-        mid = Math.floor((low + high) / 2);
-        guess = list[mid];
-        if (guess === item)
-            return mid;
-        if (guess > item) {
-            high = mid - 1;
-        }
-        else {
-            low = mid + 1;
-        }
+  let low = 0,
+    high = list.length - 1,
+    mid,
+    guess;
+  while (low <= high) {
+    mid = Math.floor((low + high) / 2);
+    guess = list[mid];
+    if (guess === item) return mid;
+    if (guess > item) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
     }
-    return null;
+  }
+  return null;
 }
-var myList = [1, 3, 5, 7, 9];
+const myList = [1, 3, 5, 7, 9];
 console.log(binarySearch(myList, 3));
 console.log(binarySearch(myList, -1));
