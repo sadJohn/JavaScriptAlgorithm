5b41652ccb103c7827354d741086f167ce6bb7b0
YohangLibet <36272025+YohangLibet@users.noreply.github.com>
2019-05-23 17:03:52 +0800

    add greedy algorithm

---
+++ Greedy/Greedy.js
@@ -0,0 +1,40 @@
+let statesNeeded = new Set(["mt", "wa", "or", "id", "nv", "ut", "ca", "az"]);
+
+const stations = new Map([
+  ["kone", new Set(["id", "nv", "ut"])],
+  ["ktwo", new Set(["wa", "id", "mt"])],
+  ["kthree", new Set(["or", "nv", "ca"])],
+  ["kfour", new Set(["nv", "ut"])],
+  ["kfive", new Set(["ca", "az"])]
+]);
+
+const finalStation = new Set();
+
+function greedy() {
+  while (statesNeeded.size !== 0) {
+    let bestStation = null;
+    let statesCovered = new Set();
+    for (const station of stations.keys()) {
+      let covered = intersect(statesNeeded, stations.get(station));
+      if (covered.size > statesCovered.size) {
+        bestStation = station;
+        statesCovered = covered;
+      }
+    }
+    statesNeeded = difference(statesNeeded, statesCovered);
+    finalStation.add(bestStation);
+  }
+}
+
+greedy();
+console.log(finalStation);
+
+//set1 & set2
+function intersect(set1, set2) {
+  return new Set([...set1].filter(i => set2.has(i)));
+}
+
+//set1 - set2
+function difference(set1, set2) {
+  return new Set([...set1].filter(i => !set2.has(i)));
+}
