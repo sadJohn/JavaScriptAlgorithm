d16e3b1014885bdd331f9c58b0ad6e13f54c1da5
YohangLibet <36272025+YohangLibet@users.noreply.github.com>
2019-06-20 11:55:23 +0800

    improve

--- BubbleSort/BubbleSort.js
+++ BubbleSort/BubbleSort.js
@@ -4,19 +4,20 @@ function swap(arr, index1, index2) {
 
 function BubbleSort(arr) {
   let sorted = false;
+  let length = arr.length;
   while (!sorted) {
     sorted = true;
-    for (let i = 1; i < arr.length; i++) {
+    for (let i = 1; i < length; i++) {
       if (arr[i - 1] > arr[i]) {
         swap(arr, i - 1, i);
         sorted = false;
       }
     }
+    length--;
   }
 }
 
-const arr = [2, 1, 3, 6, 4, 7, 5]
-console.log(arr.toString())
-BubbleSort(arr)
-console.log(arr.toString())
-
+const arr = [2, 1, 3, 6, 4, 7, 5];
+console.log(arr.toString());
+BubbleSort(arr);
+console.log(arr.toString());
