c03b758b3651d7280eedf3e3b4fc2957748af036
YohangLibet <36272025+YohangLibet@users.noreply.github.com>
2019-06-20 11:36:41 +0800

    add BubbleSort

---
+++ BubbleSort/BubbleSort.js
@@ -0,0 +1,22 @@
+function swap(arr, index1, index2) {
+  [arr[index1], arr[index2]] = [arr[index2], arr[index1]];
+}
+
+function BubbleSort(arr) {
+  let sorted = false;
+  while (!sorted) {
+    sorted = true;
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i - 1] > arr[i]) {
+        swap(arr, i - 1, i);
+        sorted = false;
+      }
+    }
+  }
+}
+
+const arr = [2, 1, 3, 6, 4, 7, 5]
+console.log(arr.toString())
+BubbleSort(arr)
+console.log(arr.toString())
+
